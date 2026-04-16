fbabc9ff11e97685801a8cb146c3f9fe0d0a6e5f
Thilak A <thilaka1003@gmail.com>
2026-04-16 23:09:57 +0530

    Add files via upload

---
+++ transition_point.py
@@ -0,0 +1,8 @@
+class Solution:
+    def transitionPoint(self, arr):
+        for i in range(len(arr)):
+            if arr[i]==1:
+                return i
+           
+        return -1
+        # Code here
